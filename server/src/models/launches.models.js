eec0169fad6ce439939ba19e1e452ed007379e05
Norman Navarro <93636275+Robotic1804@users.noreply.github.com>
2024-05-18 00:12:05 -0500

    Final Project

--- server/src/models/launches.models.js
+++ server/src/models/launches.models.js
@@ -1,39 +1,52 @@
-
 const launches = new Map();
 
 let letestFlightNumber = 100;
 
 const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-42  b',
-    customer: ['ZTM', 'NASA'],
-    upcoming: true,
-    success: true
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  target: "Kepler-42  b",
+  customer: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
 };
 
 launches.set(launch.flightNumber, launch);
 
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+
 function getAllLaunches() {
-    return Array.from(launches.values());
+  return Array.from(launches.values());
 }
 
 function addNewLaunch(launch) {
-    letestFlightNumber++;
-    launches.set(
-        letestFlightNumber, Object.assign(launch, {
-            success: true,
-            upcoming: true,
-            customer: ['Zero to Mastery', 'NASA'],
-            flightNumber: latestFlightNumber
-    }));
-    
+  letestFlightNumber++;
+  launches.set(
+    letestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customer: ["Zero to Mastery", "NASA"],
+      flightNumber: letestFlightNumber,
+    })
+  );
+}
+
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+
+  return aborted;
 }
 
 module.exports = {
-
-    launches,
-    addNewLaunch
+  existsLaunchWithId,
+  getAllLaunches,
+  addNewLaunch,
+  abortLaunchById,
 };
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -3,12 +3,14 @@ const express = require('express');
 
 const {
     httpGetAllLaunches,
-    httpAddNewLaunch
+    httpAddNewLaunch,
+    httpAbortLaunch
 } = require('./launches.controller');
 
 const launchesRouter = express.Router();
 
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewLaunch);
+launchesRouter.delete('/:id', httpAbortLaunch);
 
 module.exports = launchesRouter;
